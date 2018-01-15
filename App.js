bce4e544cebde6c02e01c6bd927377bbdf3b4186
Christopher Atoki <kingatoki@Christophers-MacBook-Air.local>
2018-01-15 15:02:34 -0500

    Added the initial layout for home screen

--- App.js
+++ App.js
@@ -1,22 +1,60 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import { Asset, AppLoading } from 'expo';
 
 import FrontPageComponent from './components/FrontPageComponent/FrontPageComponent.js';
 
+let { height, width } = Dimensions.get('window');
+
+const RootNavigator = StackNavigator({
+  Home: {
+    screen: FrontPageComponent,
+    navigationOptions: {
+      headerTitle: 'Home'
+    },
+    drawerStack: { screen: DrawerNavigation }
+  }
+});
+
 export default class App extends React.Component {
+  state = {
+    isReady: false
+  };
+
+  async _cacheResourcesAsync() {
+    const images = [
+      require('./assets/mattress.png'),
+      require('./assets/pillows.png'),
+      require('./assets/adjustable-bed.png'),
+      require('./assets/bed.png'),
+      require('./assets/sheets.png')
+    ];
+
+    const cacheImages = images.map(image => {
+      return Asset.fromModule(image).downloadAsync();
+    });
+    return Promise.all(cacheImages);
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hi Chris</Text>
-        <FrontPageComponent />
-      </View>
-    );
+    if (!this.state.isReady) {
+      return (
+        <AppLoading
+          startAsync={this._cacheResourcesAsync}
+          onFinish={() => this.setState({ isReady: true })}
+          onError={console.warn}
+        />
+      );
+    }
+    return <RootNavigator />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    marginTop: height * 0.1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
--- components/FrontPageComponent/FrontPageComponent.js
+++ components/FrontPageComponent/FrontPageComponent.js
@@ -1,11 +1,71 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  ScrollView,
+  TouchableWithoutFeedback
+} from 'react-native';
+
+let { height, width } = Dimensions.get('window');
 
 export default class FrontPageComponent extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>This is the Front Page</Text>
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <TouchableWithoutFeedback onPress={() => console.log('pressed')}>
+            <View style={styles.imageView}>
+              <Image
+                style={styles.mainImage}
+                resizeMode="cover"
+                source={require('../../assets/mattress.png')}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => console.log('pressed')}>
+            <View style={styles.imageView}>
+              <Image
+                style={styles.mainImage}
+                resizeMode="cover"
+                source={require('../../assets/pillows.png')}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => console.log('pressed')}>
+            <View style={styles.imageView}>
+              <Image
+                style={styles.mainImage}
+                resizeMode="cover"
+                source={require('../../assets/adjustable-bed.png')}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => console.log('pressed')}>
+            <View style={styles.imageView}>
+              <Image
+                style={styles.mainImage}
+                resizeMode="cover"
+                source={require('../../assets/bed.png')}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          <TouchableWithoutFeedback onPress={() => console.log('pressed')}>
+            <View style={styles.imageView}>
+              <Image
+                style={styles.mainImage}
+                resizeMode="cover"
+                source={require('../../assets/sheets.png')}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+        </ScrollView>
       </View>
     );
   }
@@ -13,9 +73,20 @@ export default class FrontPageComponent extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  mainImage: {
+    width: width,
+    height: height * 0.3
+  },
+  imageView: {
+    width: width,
+    height: height * 0.3,
+    marginVertical: '5%',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2
   }
 });
